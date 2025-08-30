11497ce19362c912b7342636be5eafa676d5d0c6
programmer-saeed <kazisaeed8056@gmail.com>
2025-08-31 03:26:06 +0600

    Added  file 01_Variable_001.cpp

---
+++ 01_Variable_001.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+{
+    int first_input, second_input;
+
+    // printf("Enter First value : ");
+    scanf("%d",&first_input);
+
+    // printf("Enter Second value : ");
+    scanf("%d",&second_input);
+
+    int Summation      = first_input + second_input;
+    int Subtraction    = first_input - second_input;
+    int Multiplication = first_input * second_input;
+    int Division       = first_input / second_input;
+    int Modulus        = first_input % second_input;
+
+    printf("Summation      = %d\n", Summation);
+    printf("Subtraction    = %d\n", Subtraction);
+    printf("Multiplication = %d\n", Multiplication);
+    printf("Division       = %d\n", Division);
+    printf("Modulus        = %d\n", Modulus);
+
+    return 0;
+
+}
